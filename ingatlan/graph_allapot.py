95fe01ce253de7ca5a0af23c75aaf68b1e19b34b
madarasz <mr.madarasz.istvan@gmail.com>
2018-12-13 12:12:35 +0100

    add regression line, cleanup

--- ingatlan/graph_allapot.py
+++ ingatlan/graph_allapot.py
@@ -1,24 +1,21 @@
-import logging
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import select
-from models import db_connect, create_table, IngatlanDB, ArDB
-
-from plotly.offline import iplot, init_notebook_mode
+from models import db_connect, IngatlanDB, ArDB
+from scipy import stats
 import plotly.plotly as py
 import plotly.graph_objs as go
-import plotly.io as pio
-import os
 import numpy as np
 import re
 
+# query database
 engine = db_connect()
 conn = engine.connect()
-
 s = select([IngatlanDB.id, IngatlanDB.alapterulet, IngatlanDB.szobak_egesz, IngatlanDB.szobak_fel, IngatlanDB.cim, IngatlanDB.ingatlan_allapota, ArDB.ar])
 s = s.where(IngatlanDB.id == ArDB.ingatlan_id)
 s = s.where(IngatlanDB.ingatlan_tipus == u'lakás').where(IngatlanDB.cim.like("%Újlipótváros%")).where(IngatlanDB.epites_tipus == u'tégla')
 s = s.where(ArDB.ar < 50000000).where(IngatlanDB.alapterulet < 100)
 result = conn.execute(s).fetchall()
+
+# data init
 terulet = []
 ar = []
 szobak = []
@@ -35,14 +32,20 @@ category_mapping = {
     None: 3
 }
 
+# prepare data
 for row in result:
     terulet.append(row['alapterulet'])
     ar.append(row['ar'])
     alkerulet = re.search(r'(?:\()(.*)(?:\))', row['cim']).group()
     colors.append(category_mapping[row['ingatlan_allapota']])
     szobak.append(10+5*(row['szobak_egesz']+row['szobak_fel']*0.5))
-    hover_text.append(alkerulet + " - Terulet: " + str(row['alapterulet']) + " - Allapot: " + str(row['ingatlan_allapota']) + " - Ar: " + str(row['ar']/1000000) + "M Ft - id: " + str(row['id'])) 
+    hover_text.append("{} - Terulet: {} - Allapot: {} - Ar: {}M Ft - id: {}".format(alkerulet, row['alapterulet'], row['ingatlan_allapota'], row['ar']/1000000, row['id'])) 
 
+# calculate regression
+slope, intercept, r_value, p_value, std_err = stats.linregress(terulet,ar)
+line = slope * np.asarray(terulet) + intercept
+
+# make graph
 data = go.Scatter( 
     x=terulet,
     y=ar,
@@ -55,8 +58,16 @@ data = go.Scatter(
             'colorscale': 'Jet'
             }
 )
-layout = go.Layout(
-    title='XIII.ker Ujlipotvaros tegla lakasok, 50M alatt'
+regression = go.Scatter(
+    x=terulet,
+    y=line,
+    mode='lines',
+    line={'color':'blue', 'width':3}
 )
-fig = go.Figure(data=[data], layout=layout)
+layout = go.Layout(
+    title='XIII.ker Ujlipotvaros tegla lakasok, 50M alatt',
+    hovermode='closest',
+    showlegend=False
+)
+fig = go.Figure(data=[data, regression], layout=layout)
 py.plot(fig)
